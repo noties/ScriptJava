24f182e50bc1f416e0bc76a67a40a32998f53659
Dimitry Ivanov <mail@dimitryivanov.ru>
2016-08-23 01:40:49 +0300

    Updated copyright headers

--- test/scriptjava/parser/StatementParserTest.java
+++ test/scriptjava/parser/StatementParserTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Dimitry Ivanov (copy@dimitryivanov.ru)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package scriptjava.parser;
 
 import org.junit.Test;
@@ -8,9 +24,6 @@ import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by Дмитрий on 15.08.2016.
- */
 public class StatementParserTest {
 
     // parser has no inner state
--- test/scriptjava/parser/StatementSubstitutionTest.java
+++ test/scriptjava/parser/StatementSubstitutionTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Dimitry Ivanov (copy@dimitryivanov.ru)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package scriptjava.parser;
 
 import org.junit.Test;
@@ -6,9 +22,6 @@ import java.util.Locale;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by Дмитрий on 15.08.2016.
- */
 public class StatementSubstitutionTest {
 
     @Test
